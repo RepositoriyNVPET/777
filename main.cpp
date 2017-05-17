3b53a8b1fe5c2134c78b64aec2b81ddd78de3412
Admin012 <roma.prihodko.1997@mail.ru>
2017-05-17 12:41:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
 	
 {
     double d1, d2, d3, dsum;
-    printf  ("\n Enter  \n");
+    printf  ("\n Enter price \n");
     scanf  (" %f%f%f ",  &d1, &d2, &d3);
     dsum = d1+ d2 + d3;
     printf  (" %f + %f + %f = %f ", d1, d2, d3, dsum);
